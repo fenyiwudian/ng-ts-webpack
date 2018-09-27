c493a38b04976ff41d6f992fd553f36a008cad2e
fenyiwudian <674702221@qq.com>
2018-09-27 10:39:41 +0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+// 基于node的 遵循commonjs规范的
+let path = require('path');//node的模块
+module.exports = {
+    entry: './src/index.js', // 入口
+    output: {
+        filename: 'build.js',
+        // 这个路径必须是绝对路径
+        path: path.resolve('./dist')
+    }, // 出口
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        host: 'localhost',
+        compress: true,
+        port: 8080
+    },
+
+    module: {}, // 模块配置
+    plugins: [], // 插件的配置
+    mode: 'development', // 可以更改模式
+    resolve: {}, // 配置解析
+}
+// 在webpack中如何配置开发服务器 webpack-dev-server
