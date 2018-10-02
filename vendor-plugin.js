aecd49fdf5b1486978a781616eafb03c3b718dee
fenyiwudian <674702221@qq.com>
2018-10-02 22:42:44 +0800

    insert vendor script tag via vendor plugin

--- vendor-plugin.js
+++ vendor-plugin.js
@@ -1,26 +1,45 @@
 const fs = require('fs');
 const hasha = require('hasha');
-class LangPlugin {
+class VendorPlugin {
     constructor(options) {
         this.options = options || {};
     }
     apply(compiler) {
-        const { options, local} = this;
+        const { options, local } = this;
+
+        const dataList = [];
+        Object.keys(options).forEach(key => {
+            const list = options[key];
+            let content = list.reduce((rs, file) => {
+                rs += fs.readFileSync(file).toString() + '\n//////\n';
+                return rs;
+            }, '');
+            const hash = hasha(content).substr(0, 8);
+            const fileName = local ? `${key}.js` : `${key}-${hash}.js`;
+            dataList.unshift({fileName, content});
+        });
+
+        compiler.hooks.compilation.tap('VendorPlugin', (compilation) => {
+            compilation.hooks.htmlWebpackPluginAfterHtmlProcessing.tapAsync(
+                'VendorPlugin',
+                (data, cb) => {
+                    dataList.forEach(temp => {
+                        data.html = data.html.replace('</body>', 
+                        `<script src="${temp.fileName}"></script></body>`);
+                    });
+                    cb(null, data);
+                }
+            );
+        });
+
         compiler.plugin('emit', function (compilation, callback) {
-            Object.keys(options).forEach(key => {
-                const list = options[key];
-                let content = list.reduce((rs, file) => {
-                    rs += fs.readFileSync(file).toString() + '\n//////\n';
-                    return rs;
-                }, '');
-                const hash = hasha(content).substr(0, 8);
-                const fileName = local ? `${key}.js` : `${key}-${hash}.js`;
-                compilation.assets[fileName] = {
+            dataList.forEach(data => {
+                compilation.assets[data.fileName] = {
                     source() {
-                        return content;
+                        return data.content;
                     },
                     size() {
-                        return content.length;
+                        return data.content.length;
                     }
                 };
             });
@@ -29,4 +48,4 @@ class LangPlugin {
     }
 }
 
-module.exports = LangPlugin;
+module.exports = VendorPlugin;
