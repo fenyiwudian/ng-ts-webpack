125dca87b415e5a9fe6503d3a2d5692e6ee63674
fenyiwudian <674702221@qq.com>
2018-10-05 14:13:58 +0800

    access cookies in preload lang module

--- src/lang.ts
+++ src/lang.ts
@@ -1,12 +1,29 @@
-console.log('init lang module start');
+// 其他模块不要依赖该模块,该模块也不依赖其他模块
+// 这是一个单独的多语言预加载模块
 
 const langSrc: any = {
   'en-US': require('../lang/en-US'),
   'zh-CN': require('../lang/zh-CN'),
 };
 
+const getCookieItem = function (sKey: string) {
+  if (!sKey) { return null; }
+  return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
+};
+
+const getLangCode = function () {
+  let rs = '';
+  try {
+    const session: any = JSON.parse(getCookieItem('session'));
+    rs = session.user.locale;
+  } catch (e) {
+
+  }
+  return rs || 'zh-CN';
+};
+
 const req = new XMLHttpRequest();
-const code = localStorage.getItem('lang-code') || 'zh-CN';
+const code = getLangCode();
 req.open('GET', 'lang-assets-host-placeholder' + langSrc[code].default);
 req.onload = () => {
   window.LANG = {
@@ -18,13 +35,8 @@ req.onload = () => {
   const script = document.createElement('script');
   script.setAttribute('type', 'text/javascript');
   script.setAttribute('src', 'bundle.js');
-  script.onload = () => {
-    console.log('bundle loaded');
-  };
   document.body.appendChild(script);
-  console.log('loading bundle');
 };
 req.send();
 
-console.log('init lang module end');
 
